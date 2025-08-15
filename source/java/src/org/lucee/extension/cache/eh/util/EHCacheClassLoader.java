484850a58368d8eb6e0bb00d3c0bbf29ec17375f
Michael Offner <michael@lucee.org>
2025-08-15 16:18:47 +0200

    clean up

--- source/java/src/org/lucee/extension/cache/eh/util/EHCacheClassLoader.java
+++ source/java/src/org/lucee/extension/cache/eh/util/EHCacheClassLoader.java
@@ -5,19 +5,19 @@ import java.lang.reflect.Method;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
+import lucee.commons.io.log.Log;
 import lucee.loader.engine.CFMLEngine;
 import lucee.loader.engine.CFMLEngineFactory;
 import lucee.runtime.config.Config;
-
-import lucee.commons.io.log.Log;
-
+import lucee.runtime.util.ClassUtil;
 
 /**
- * A special class loader that first attempts to load class from the Lucee Environment,
- * but that can fail when RMI classes are requested via a spawned thread, so when
- * the class cannot be located in the Lucee Environment, it searches all the Felix
- * bundles for the requested class. This will prevent issues with issues trying
- * to load classes like net.sf.ehcache.distribution.RMICachePeer_Stub. 
+ * A special class loader that first attempts to load class from the Lucee
+ * Environment, but that can fail when RMI classes are requested via a spawned
+ * thread, so when the class cannot be located in the Lucee Environment, it
+ * searches all the Felix bundles for the requested class. This will prevent
+ * issues with issues trying to load classes like
+ * net.sf.ehcache.distribution.RMICachePeer_Stub.
  *
  * @author Dan G. Switzer, II
  * @version 1.0
@@ -32,24 +32,24 @@ public class EHCacheClassLoader extends ClassLoader {
 		try {
 			Method m = config.getClass().getMethod("getClassLoaderEnv", new Class[0]);
 			this.coreEnvClassLoader = (ClassLoader) m.invoke(config, new Object[0]);
-		}
-		catch (Exception e) {
+		} catch (Exception e) {
 			// do nothing
 		}
 	}
 
 	/*
-	 * We need to test if the class is a framework bundle and to do that, we need
-	 * to use reflection to get access to the private OSGiUtil isFrameworkBundle()
+	 * We need to test if the class is a framework bundle and to do that, we need to
+	 * use reflection to get access to the private OSGiUtil isFrameworkBundle()
 	 * method.
 	 * 
-	 * IMPORTANT — If the method in the Lucee source code changes, this function will break.
+	 * IMPORTANT — If the method in the Lucee source code changes, this function
+	 * will break.
 	 */
-	public boolean isFrameworkBundle(Bundle b){
+	public boolean isFrameworkBundle(Bundle b) {
 		try {
-			Class<?> clazz = CFMLEngineFactory.getInstance().getClassUtil().loadClass("lucee.runtime.osgi.OSGiUtil");
-			Method m = clazz.getMethod("isFrameworkBundle", new Class[] { Bundle.class });
-			return (boolean) m.invoke(null, b);
+			ClassUtil util = CFMLEngineFactory.getInstance().getClassUtil();
+			return (boolean) util.callStaticMethod(util.loadClass("lucee.runtime.osgi.OSGiUtil"), "isFrameworkBundle",
+					new Object[] { b });
 		} catch (Exception e) {
 			return false;
 		}
@@ -61,6 +61,7 @@ public class EHCacheClassLoader extends ClassLoader {
 		return logger;
 	}
 
+	@Override
 	public Class<?> loadClass(final String className) throws ClassNotFoundException {
 		Class<?> clazz = null;
 
@@ -69,19 +70,19 @@ public class EHCacheClassLoader extends ClassLoader {
 		log.debug("ehcache", "Loading class " + className);
 
 		// we should try the finding the class in the Lucee environmental class loader
-		if( this.coreEnvClassLoader != null ){
+		if (this.coreEnvClassLoader != null) {
 			try {
-				clazz = (Class<?>)this.coreEnvClassLoader.loadClass(className);
+				clazz = this.coreEnvClassLoader.loadClass(className);
 			} catch (ClassNotFoundException ex) {
 				// ignore that we cannot find the class
 				log.debug("ehcache", "Could not find " + className + " class in EnvClassLoader.");
 			}
 		}
-		
+
 		log.debug("ehcache", "Could not find " + className + " in normal class loader, searching Felix bundles...");
 		clazz = searchFelixBundleClasses(className);
 
-		if( clazz == null ){
+		if (clazz == null) {
 			log.debug("ehcache", "Could not find " + className + " class in any class loader!");
 			throw new ClassNotFoundException();
 		}
@@ -89,10 +90,10 @@ public class EHCacheClassLoader extends ClassLoader {
 		return clazz;
 	}
 
-	private Class<?> searchFelixBundleClasses(final String className){
+	private Class<?> searchFelixBundleClasses(final String className) {
 		Class<?> clazz = null;
 
-		synchronized (getClassLoadingLock(className)){		
+		synchronized (getClassLoadingLock(className)) {
 			// try to find the class in the Felix bundles
 			CFMLEngine engine = CFMLEngineFactory.getInstance();
 
@@ -107,12 +108,12 @@ public class EHCacheClassLoader extends ClassLoader {
 						if (b != null && !isFrameworkBundle(b)) {
 							try {
 								clazz = b.loadClass(className);
-							}
-							catch (Exception e) {
+							} catch (Exception e) {
 								clazz = null;
 							}
-							if( clazz != null ){
-								getLogger().debug("ehcache", "Found in " + className + " class in " + b.getSymbolicName() + " bundle.");
+							if (clazz != null) {
+								getLogger().debug("ehcache",
+										"Found in " + className + " class in " + b.getSymbolicName() + " bundle.");
 								return clazz;
 							}
 						}
@@ -123,7 +124,7 @@ public class EHCacheClassLoader extends ClassLoader {
 
 		return clazz;
 	}
-	
+
 	public ClassLoader getClassLoader() {
 		return EHCacheClassLoader.class.getClassLoader();
 	}
